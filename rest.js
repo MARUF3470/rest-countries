206578e1a8073097ebf595fa8c23ea930b2ebcdc
MARUF3470 <smmaruf25@gmail.com>
2022-09-19 02:26:32 +0600

    done

--- rest.js
+++ rest.js
@@ -4,7 +4,20 @@ const LoadCountries = () => {
         .then(data => displayCountries(data))
 }
 const displayCountries = countries => {
-
+    //console.log(countries)
+    const allCountryHtml = countries.map(country => getCountryHtml(country))
+    // console.log(allCountryHtml)
+    const container = document.getElementById('countries')
+    container.innerHTML = allCountryHtml.join(' ')
+}
+const getCountryHtml = country => {
+    //console.log(country.name.common)
+    return `
+    <div class="country">
+        <h2>${country.name.common}</h2>
+        <img src = '${country.flags.png}'>
+    </div>
+    `
 }
 
 LoadCountries()
