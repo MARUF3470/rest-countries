87cc689f734b8f9c948f7fa2c51d342d66a54b47
MARUF3470 <smmaruf25@gmail.com>
2022-09-19 01:26:21 +0600

    first commit

---
+++ rest.js
@@ -0,0 +1,10 @@
+const LoadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+const displayCountries = countries => {
+
+}
+
+LoadCountries()
